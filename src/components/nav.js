728ddf7a5c5962510b70ea03d6db35a054689cd9
Daniel Escobar <escobard90@gmail.com>
2017-01-04 23:46:14 -0700

    type: feat
    added navbar to application

--- src/components/nav.js
+++ src/components/nav.js
@@ -3,7 +3,22 @@ import React, { Component } from 'react';
 export default class Nav extends Component {
   render() {
     return (
-    	<p>test</p>
+		<nav className="navbar navbar-dark bg-primary navbar-fixed-top">
+
+		    <div className="nav-container">
+
+		        <div className="navbar-toggleable-xs" id="collapseEx2">
+		            <a className="navbar-brand">Navbar</a>
+		            <ul className="nav navbar-nav">
+		                <li className="nav-item active">
+		                    <a className="nav-link">Home <span className="sr-only">(current)</span></a>
+		                </li>
+		            </ul>
+		        </div>
+
+		    </div>
+
+		</nav>
     );
   }
 }
