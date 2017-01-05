7201a0bed44d28d5adc5c0d3cb33bd40d99237a8
Daniel Escobar <escobard90@gmail.com>
2017-01-04 23:50:36 -0700

    type: feat
    styled navbar

--- src/components/app.js
+++ src/components/app.js
@@ -8,7 +8,7 @@ export default class App extends Component {
     return (
     <div>
     <Nav />
-    <section>
+    <section className="container">
       <SearchBar />
       <WeatherList />
      </section>
--- src/components/nav.js
+++ src/components/nav.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class Nav extends Component {
   render() {
     return (
-		<nav className="navbar navbar-dark bg-primary navbar-fixed-top">
+		<nav role="navigation" className="navbar navbar-dark bg-primary navbar-fixed-top">
 
 		    <div className="nav-container">
 
