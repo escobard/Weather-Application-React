921c15e5822a6898d20eebc998bc814e0894067e
Daniel Escobar <escobard90@gmail.com>
2017-01-05 00:04:51 -0700

    type: fix
    fixed missing MDB dependencies

--- src/components/nav.js
+++ src/components/nav.js
@@ -5,20 +5,18 @@ export default class Nav extends Component {
     return (
 		<nav role="navigation" className="navbar navbar-dark bg-primary navbar-fixed-top">
 
-		    <div className="nav-container">
+		    <div className="container nav-container">
 
 		        <div className="navbar-toggleable-xs" id="collapseEx2">
-		            <a className="navbar-brand">Navbar</a>
-		            <ul className="nav navbar-nav">
-		                <li className="nav-item active">
-		                    <a className="nav-link">Home <span className="sr-only">(current)</span></a>
-		                </li>
-		            </ul>
+		            <h1 className="navbar-brand">Weather Predicter - Capstone Project -v0.1</h1>
 		        </div>
 
 		    </div>
 
 		</nav>
+
+
+
     );
   }
 }
