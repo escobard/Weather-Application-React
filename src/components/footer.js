53271a19543f320ae544fc1d2a94dbafcec1c42e
Daniel Escobar <escobard90@gmail.com>
2017-01-05 00:59:04 -0700

    type: fix
    fixed footer semantics

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,33 +3,33 @@ import React, { Component } from 'react';
 export default class FooterComp extends Component {
   render() {
     return (
-        <div class="container-fluid">
-            <div class="row">
+        <div>
+            <div className="container-fluid">
+                <div className="row">
 
-                <div class="col-md-6">
-                    <h5 class="title">Footer Content</h5>
-                    <p>Here you can use rows and columns here to organize your footer content.</p>
-                </div>
+                    <div className="col-md-6">
+                        <h5 className="title">Footer Content</h5>
+                        <p>Here you can use rows and columns here to organize your footer content.</p>
+                    </div>
 
-                <div class="col-md-6">
-                    <h5 class="title">Links</h5>
-                    <ul>
-                        <li><a href="#!">Link 1</a></li>
-                        <li><a href="#!">Link 2</a></li>
-                        <li><a href="#!">Link 3</a></li>
-                        <li><a href="#!">Link 4</a></li>
-                    </ul>
+                    <div className="col-md-6">
+                        <h5 className="title">Links</h5>
+                        <ul>
+                            <li><a href="#!">Link 1</a></li>
+                            <li><a href="#!">Link 2</a></li>
+                            <li><a href="#!">Link 3</a></li>
+                            <li><a href="#!">Link 4</a></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
-        
-        <div class="footer-copyright">
-            <div class="container-fluid">
-                <span>© 2015 Copyright:</span>
-                <a href="http://www.MDBootstrap.com"> MDBootstrap.com </a>
+            <div className="footer-copyright">
+                <div className="container-fluid">
+                    <span>© 2015 Copyright:</span>
+                    <a href="http://www.MDBootstrap.com"> MDBootstrap.com </a>
+                </div>
             </div>
         </div>
-        </div>
-        
     );
   }
 }
