c307c7412636d05df315d58047613fb8b15c25b9
Daniel Escobar <escobard90@gmail.com>
2017-01-18 02:11:40 -0700

    type: feat
    enabled and updated service-worker

--- service-worker.js
+++ service-worker.js
@@ -6,7 +6,7 @@
 
 
 // sets the cache name
-var staticCacheName = 'wa-static-v0.8';
+var staticCacheName = 'wa-static-v1.0';
 
 self.addEventListener('install', function(event) {
   event.waitUntil(
@@ -15,6 +15,9 @@ self.addEventListener('install', function(event) {
       '/',
       '/style/style.css',
       '/style/mdb.min.css',
+      '/img/clouds-192.png',
+      '/img/clouds.png',
+      '/img/clouds-favicon.png',
       '/bundle.js'
       ]);
     })
