33e2a2c9381dfc3991a85a2235b251e6b881c186
Daniel Escobar <escobard90@gmail.com>
2017-01-17 23:10:36 -0700

    type: feat
    added delayed timout for googlemap

--- src/containers/geo_location.js
+++ src/containers/geo_location.js
@@ -7,20 +7,39 @@ import { connect } from 'react-redux';
 // imports actions
 import getLocation from '../actions/action_geolocation';
 
+// imports gmap
+import GoogleMap from '../components/google_map';
 
 class Location extends Component {
   componentWillMount() {
     this.props.getLocation();
   }
+  fetchCoordinate(){
 
-  render () {
-    const {coords: {latitude, longitude}} = this.props.location;
+    var latitude = this.props.location.coords.latitude;
+    var longitude = this.props.location.coords.longitude;
     
     return (
       <div>
         <div>Latitude: <span>{latitude}</span></div>
         <div>Longitude: <span>{longitude}</span></div>
       </div>
+    );
+  }
+  fetchMapCoords(){
+  // only able to do this with a set-timeout unfortunately
+  setTimeout(() => {
+    const {coords: {latitude, longitude}} = this.props.location;
+    console.log("LAT" + latitude);
+    console.log("LON" + longitude);
+    },500);
+  }
+  render () {
+    return (
+    <div> 
+        {this.fetchCoordinate()}
+        {this.fetchMapCoords()}
+    </div>
     );
   }
 }
