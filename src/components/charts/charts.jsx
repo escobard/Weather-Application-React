661e3353ddc823d843b14fdf9d9049cd707b8877
Daniel Escobar <escobard90@gmail.com>
2017-02-04 16:09:06 -0700

    type: feat
    added charts component to refactor all charts into a single re-usable component

---
+++ src/components/charts/charts.jsx
@@ -0,0 +1,56 @@
+// =============================================================
+// 
+// 	charts/charts/jsx
+//
+// =============================================================
+
+import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import ChartLines from './chart_lines';
+import ChartBars from './chart_bars';
+import ChartSpots from './chart_spots';
+import GoogleMap from '../google_map';
+
+
+export default class WeatherList extends Component {
+	render(){
+
+		// sets our coordinate data
+		const lat = this.props.lat;
+		const lon = this.props.lon;
+
+		// sets our city data
+		const cityName = this.props.cityName;
+		const temps = this.props.temps;
+		console.log(temps);
+		const pressures = this.props.pressures;
+		console.log(pressures);
+		const humidities = this.props.humidities;
+		console.log(humidities);
+
+		return(
+			<article className="card animated fadeInDown" key={cityName}>
+				    <div className="card-block">
+				        <h4 className="card-title animated fadeInDown">Forecast for {cityName}</h4>
+				    </div>
+				    <section className="animated fadeInUp mapContainer">
+				    	<GoogleMap zoom={12} lon={lon} lat={lat} />
+				    </section>
+					<section className="charts">
+						<div className="chartContainer first animated fadeInUp">
+							<ChartLines chartData={temps} color="#FF5200" units="Kelvin"/>
+						</div>
+						<div className="chartContainer second animated fadeInUp">
+							<ChartBars chartData={humidities} color="#00FF6A" units="%"/>
+						</div>
+						<div className="chartContainer third animated fadeInUp">
+							<ChartSpots chartData={pressures} color="#FF6E00" units="%"/>
+						</div>
+					</section>		
+			</article>
+		);
+	}
+
+}
