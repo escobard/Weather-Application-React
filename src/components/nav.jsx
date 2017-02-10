c8eb6eb4f51d0f780738da491853cc7a884530b0
Daniel Escobar <escobard90@gmail.com>
2017-02-09 21:55:10 -0700

    type: style
    animated cloud on title

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -15,7 +15,7 @@ export default class Nav extends Component {
 		    <div className="container nav-container">
 
 		        <div className="navbar-toggleable-xs" id="collapseEx2">
-		            <h1 className="navbar-brand animated slideInDown"><img src="img/clouds.png" alt="weather icon"/>Weather Application - v0.97</h1>
+		            <h1 className="navbar-brand animated slideInDown"><img className="animated rubberBand" src="img/clouds.png" alt="weather icon"/>Weather Application - v0.97</h1>
 		        </div>
 
 		    </div>
